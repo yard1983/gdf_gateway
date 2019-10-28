05fdfa1222ed39827c31bcf0f5e04e34428a3912
Jesus Gonzalez <jesus.gonzalez@genesys.com>
2019-10-28 16:18:39 -0400

    version 18

--- server.js
+++ server.js
@@ -74,10 +74,14 @@ server.post(endpoint + "intent", function (req, resMain, next) {
   return next();
 });
 
-server.get(/\/?.*/, restify.plugins.serveStatic({
-  directory: './public',
-  default: 'index.html'
-}));
+server.get('/\/.*/', restify.plugins.serveStatic({
+
+    directory: __dirname + "/public/",
+    default: './index.html' 
+
+   })
+);
+
 
 // Start the server:
 server.listen(config.port, function () {
