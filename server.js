4e32390a0a90cc66e5c39d4aae7e767257e97906
Jesus Gonzalez <jesus.gonzalez@genesys.com>
2019-10-28 13:59:39 -0400

    modif 5

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ var endpoint = '/gdf/';
 
 server.use(restify.plugins.bodyParser());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 server.post(endpoint + "intent", function (req, resMain, next) {
 
   if (req.body.query) {
