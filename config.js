1f2707181ef070d63f53417f8da57fa39b2a32ec
Jesus Gonzalez <jesus.gonzalez@genesys.com>
2019-10-28 12:25:35 -0400

    Initial

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: '0.0.0.0',
+	env: process.env.NODE_ENV || 'development',
+	port: process.env.PORT || 3000,
+	base_url: process.env.BASE_URL || 'http://localhost:3000',
+	projectId: 'nexa-beyvqc',
+	appCredentials: 'nexa-beyvqc-52fa3089d3b8.json',
+};
