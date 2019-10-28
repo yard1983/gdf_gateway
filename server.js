a6bb04885a7ad11412c21e99519f394e826217a3
Jesus Gonzalez <jesus.gonzalez@genesys.com>
2019-10-28 12:57:25 -0400

    Initial 4

--- server.js
+++ server.js
@@ -1,125 +1,71 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+var config = require('./config');
+var restify = require('restify');
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+process.env.GOOGLE_APPLICATION_CREDENTIALS=config.appCredentials;
 
-if (mongoURL == null) {
-  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
+var server = restify.createServer({
+    name: config.name,
+    version: '1.0.0'
+});
 
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
-    }
-  }
+var endpoint = '/gdf/';
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-  }
-}
-var db = null,
-    dbDetails = new Object();
+server.use(restify.plugins.bodyParser());
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+server.post(endpoint + "intent", function (req, resMain, next) {
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+  if (req.body.query) {
+    const sessionId = uuid.v4();
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient();
+    const sessionPath = sessionClient.sessionPath(config.projectId, sessionId);
+    let promise;
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: req.body.query,
+          languageCode: 'en-US',
+        },
+      },
+    };
+    console.log('Sending query: ' + req.body.query);
+    promise = sessionClient.detectIntent(request);
 
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
+    promise
+      .then(responses => {
+        console.log('Detected intent');
+        const result = responses[0].queryResult;
+        console.log(`  Query: ${result.queryText}`);
+        console.log(`  Response: ${result.fulfillmentText}`);
+        if (result.intent) {
+          console.log(`  Intent: ${result.intent.displayName}`);
+        } else {
+          console.log(`  No intent matched.`);
+        }
+        resMain.send({ response: "OK",
+          query: result.queryText,
+          response: result.fulfillmentText,
+          intent: result.intent.displayName,
+          confidence: result.intentDetectionConfidence
+        });
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+      resMain.send(400, { response: "Incorrect JSON structure" });
   }
+  return next();
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
+// Start the server:
+server.listen(config.port, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
